ffb8d48200fbecd90eff7995088327125db855f4
nunesfariasg <nunesfariasgustavo@gmail.com>
2026-09-04 19:42:17 -0300

    feat(01-http): Implement httpserver and httpclient for POST requests

---
+++ experiments/01-http/HttpClient66.java
@@ -0,0 +1,66 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpClient66 {
+    public static void main(String[] args) {
+
+        // Creates the http client
+        HttpClient client = HttpClient.newHttpClient();
+
+        getRequest(client);
+        postRequest(client);
+    }
+
+    private static void getRequest(HttpClient client) {
+
+        try {
+
+            // Configure the request to the server address
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:6666/api/helloworld"))
+                    .GET() // Define the method
+                    .build();
+
+            // Sends the request and receives the response from the server
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Displays the results in the console
+            System.out.println("Status code: " + response.statusCode());
+            System.out.println("Response content: " + response.body());
+
+
+        } catch (Exception exception) {
+            System.err.println("Error in GET request" + exception.getMessage());
+        }
+    }
+
+    private static void postRequest(HttpClient client) {
+
+        try {
+
+            // Data to be sand
+            String dataToSand = "name: mateus, age: 19";
+
+            //configure the request to the server address
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:6666/api/sand"))
+                    .headers("Content-Type", "text/plain") // Data type to be sand
+                    .POST(HttpRequest.BodyPublishers.ofString(dataToSand)) // Passes the request body
+                    .build();
+
+            // Sand and wait for the response
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Displays what the server response
+            System.out.println("Status code: " + response.statusCode());
+            System.out.println("Server response: " + response.body());
+
+        }catch (Exception exception) {
+            System.err.println("Error in POST request" + exception.getMessage());
+        }
+
+
+    }
+}
---
+++ experiments/01-http/HttpServer66.java
@@ -0,0 +1,76 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+public class HttpServer66 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Creates the server on port 6666
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(6666),
+                0
+        );
+
+        // Route to GET requests
+        server.createContext("/api/helloworld", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange httpExchange) throws IOException {
+
+                // Response message
+                String response = "Hello, World!\n";
+
+                // Configure headers with status and message size
+                httpExchange.sendResponseHeaders(200, response.getBytes().length);
+
+                // Write text in the message
+                OutputStream outputStream = httpExchange.getResponseBody();
+                outputStream.write(response.getBytes());
+                outputStream.close();
+            }
+        });
+
+        // Route to POST requests
+        server.createContext("/api/sand", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange httpExchange) throws IOException {
+
+                // Checks if method sanded is POST
+                if ("POST".equalsIgnoreCase(httpExchange.getRequestMethod())) {
+
+                    // Reads body sanded for the client
+                    InputStream inputStream = httpExchange.getRequestBody();
+                    String bodyReceived = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                    System.out.println("Service received the data: " + bodyReceived);
+
+                    // Create a confirmation response
+                    String response = "Server processed the POST with success!";
+
+                    // Sands headers with status and message size
+                    httpExchange.sendResponseHeaders(200,response.getBytes().length);
+
+                    // Sands the response message
+                    OutputStream outputStream = httpExchange.getResponseBody();
+                    outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+                    outputStream.close();
+
+                }
+                else {
+                    // If it is not a POST request, returns a 405 error
+                    httpExchange.sendResponseHeaders(405,-1);
+                }
+            }
+        });
+
+        // Starts the server
+        server.setExecutor(null);// Creates a default executor
+        System.out.println("Server running on port 6666...");
+        server.start();
+    }
+}
